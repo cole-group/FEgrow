7a8f2cf1733d08094b8c56c6d9eaef82eb87cf8d
bieniekmat <bieniekmat@gmail.com>
2021-11-16 13:20:24 +0000

    The installation file for R-group. Version now in the setup.py

--- rgroup/__init__.py
+++ rgroup/__init__.py
@@ -6,5 +6,3 @@ from .toxicity import tox_props
 RGroups = RGroupGrid()
 
 __all__ = [RMol, generate_conformers, rep2D, fix_receptor, optimise_in_receptor, tox_props, sort_conformers, RGroups, build_molecules, ic50]
-
-from .version import __version__
---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='rgroup',
+    version='0.0.1.dev1',
+    description='RGroup: generate congeneric ligands by modifying groups. ',
+    long_description='Copy from README file',
+    url='https://blogs.ncl.ac.uk/danielcole/',
+    author='Mateusz K. Bieniek, Ben Cree, Rachael Pirie, Josh Horton, Daniel Cole',
+    author_email='bieniekmat@gmail.com',
+    install_requires=['parmed', 'tqdm', 'mdanalysis', 'typing-extensions'],
+    packages=find_packages(),
+    include_package_data=True,
+)
