befe6a0d7dfa60e9c444b34b6a1ee5a4dc164012
bieniekmat <bieniekmat@gmail.com>
2022-05-05 16:23:50 +0100

    RLinkers nitty gritty

--- fegrow/__init__.py
+++ fegrow/__init__.py
@@ -1,8 +1,10 @@
 from .conformers import generate_conformers
-from .package import RList, RMol, rep2D, build_molecules, ic50, RGroupGrid
+from .package import RList, RMol, rep2D, build_molecules, ic50, RGroupGrid, RLinkerGrid, link
 from .receptor import fix_receptor, optimise_in_receptor, sort_conformers
 from .toxicity import tox_props
 
 RGroups = RGroupGrid()
+RLinkers = RLinkerGrid()
 
-__all__ = [RMol, generate_conformers, rep2D, fix_receptor, optimise_in_receptor, tox_props, sort_conformers, RGroups, build_molecules, ic50]
+__all__ = [RMol, generate_conformers, rep2D, fix_receptor, optimise_in_receptor, tox_props, sort_conformers,
+           RGroups, RLinkers, link, build_molecules, ic50]
