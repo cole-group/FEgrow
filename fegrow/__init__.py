f1e1aeaba87801bd37c503a23285519658dc26d2
bieniekmat <bieniekmat@gmail.com>
2022-05-18 16:04:38 +0100

    version file

--- fegrow/__init__.py
+++ fegrow/__init__.py
@@ -1,9 +1,10 @@
 from pathlib import Path
 
 from .conformers import generate_conformers
-from .package import RList, RMol, rep2D, build_molecules, ic50, RGroupGrid, RLinkerGrid, link
+from .package import RList, RMol, rep2D, build_molecules, ic50, RGroupGrid, RLinkerGrid
 from .receptor import fix_receptor, optimise_in_receptor, sort_conformers
 from .toxicity import tox_props
+from .version import __version__
 
 RGroups = RGroupGrid()
 RLinkers = RLinkerGrid()
@@ -12,8 +13,7 @@ __version__ = open(Path(__file__).parent / 'version.txt').read().strip()
 
 __all__ = [RMol,
            generate_conformers,
-           rep2D,
-           fix_receptor,
+           rep2D, fix_receptor,
            optimise_in_receptor,
            tox_props,
            sort_conformers,
