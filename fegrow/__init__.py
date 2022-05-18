7fa4b99e0ff3d14815fc984e366dc01aa05eeb85
bieniekmat <bieniekmat@gmail.com>
2022-05-18 16:28:34 +0100

    Versioning.

--- fegrow/__init__.py
+++ fegrow/__init__.py
@@ -2,7 +2,17 @@ from .conformers import generate_conformers
 from .package import RList, RMol, rep2D, build_molecules, ic50, RGroupGrid
 from .receptor import fix_receptor, optimise_in_receptor, sort_conformers
 from .toxicity import tox_props
+from .version import __version__
 
 RGroups = RGroupGrid()
 
-__all__ = [RMol, generate_conformers, rep2D, fix_receptor, optimise_in_receptor, tox_props, sort_conformers, RGroups, build_molecules, ic50]
+__all__ = [RMol,
+           generate_conformers,
+           rep2D, fix_receptor,
+           optimise_in_receptor,
+           tox_props,
+           sort_conformers,
+           RGroups,
+           build_molecules,
+           ic50,
+           __version__]
