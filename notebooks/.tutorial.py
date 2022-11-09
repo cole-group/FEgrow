0ef65b1a68c8054572e5a45f3314bebd930f43c5
Mateusz Bieniek <bieniekmateusz@users.noreply.github.com>
2022-11-09 15:56:25 +0000

    Update .tutorial.py

--- notebooks/.tutorial.py
+++ notebooks/.tutorial.py
@@ -16,3 +16,23 @@ R_group_cyclopropane = groups.loc[groups['Name'] == '*C1CC1' ]['Mol'].values[0]
 R_group_propanol = Chem.MolFromMolFile('manual_rgroups/propan-1-ol-r.mol', removeHs=False)
 selected_rgroups = [R_group_ethanol] + interactive_rgroups
 selected_rgroups
+
+rmols = fegrow.build_molecules(template_mol, 
+                               attachment_index, 
+                               selected_rgroups)
+rmols
+rmols.rep2D()
+rmols[0].rep3D()
+rmols.toxicity()
+rmols.generate_conformers(num_conf=5,
+                          minimum_conf_rms=0.5, 
+                          # flexible=[3, 18, 20])
+                        )
+sys = prody.parsePDB('7L10.pdb')
+rec = sys.select('not (nucleic or hetatm or water)')
+prody.writePDB('rec.pdb', rec)
+fegrow.fix_receptor("rec.pdb", "rec_final.pdb")
+rec_final = prody.parsePDB("rec_final.pdb")
+rmols[0].rep3D(prody=rec_final)
+rmols.remove_clashing_confs(rec_final)
+rmols[0].rep3D(prody=rec_final)
