53f9954e94437d9468e69f2f28b3363e0e2cbd7c
bieniekmat <bieniekmat@gmail.com>
2023-02-23 16:42:57 +0000

    Formatting

--- fegrow/package.py
+++ fegrow/package.py
@@ -732,7 +732,7 @@ class RLinkerGrid(mols2grid.MolGrid):
                     ),  # extract the index property from the original publication
                 }
             )
-            
+
         # presort using the original publication index
         linkers = sorted(linkers, key=lambda i: i["Common"])
 
