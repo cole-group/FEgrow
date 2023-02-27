a15a204eb63ad7e2afa94fe9dca0848ac213d87f
Mateusz Bieniek <bieniekmateusz@users.noreply.github.com>
2023-02-27 15:19:46 +0000

    Faster CI jupyter notebook

--- notebooks/.tutorial.py
+++ notebooks/.tutorial.py
@@ -128,13 +128,12 @@ interactive_rgroups = RGroups.get_selected()
 # you can also directly access the built-in dataframe programmatically
 groups = RGroups.dataframe
 R_group_ethanol = groups.loc[groups['Name']=='*CCO']['Mol'].values[0]
-R_group_cyclopropane = groups.loc[groups['Name'] == '*C1CC1' ]['Mol'].values[0]
 
 # add your own R-groups files
 R_group_propanol = Chem.MolFromMolFile('manual_rgroups/propan-1-ol-r.mol', removeHs=False)
 
 # make a list of R-group molecule
-selected_rgroups = [R_group_propanol, R_group_ethanol, R_group_cyclopropane] + interactive_rgroups
+selected_rgroups = [R_group_propanol, R_group_ethanol] + interactive_rgroups
 selected_rgroups
 
 
@@ -191,7 +190,7 @@ rmols.toxicity()
 # In[ ]:
 
 
-rmols.generate_conformers(num_conf=50, 
+rmols.generate_conformers(num_conf=10, 
                           minimum_conf_rms=0.5, 
                           # flexible=[3, 18, 20])
                         )
