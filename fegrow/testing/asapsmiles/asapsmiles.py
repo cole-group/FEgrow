be9f69dd08ad377e36fd9ea29d75929adc3b9c06
bieniekmat <bieniekmat@gmail.com>
2023-06-02 10:33:03 +0100

    energies, smiles with hydrogens

--- fegrow/testing/asapsmiles/asapsmiles.py
+++ fegrow/testing/asapsmiles/asapsmiles.py
@@ -1,18 +1,48 @@
-from fegrow import RList
+from fegrow import RList, RMol
 from rdkit import Chem
 import prody
 
 
-# create a list of molecules from smiles
-rdkit_mols = Chem.SmilesMolSupplier('sars_generated_smiles.txt')
 # load the common core
 core = Chem.SDMolSupplier('core.sdf')[0]
 
-rlist = RList.from_mols(rdkit_mols)
-rlist.set_common_core(core)
+# create RList of molecules from smiles
+rlist = RList()
+params = Chem.SmilesParserParams()
+params.removeHs = False # keep the hydrogens
+for smiles in open('sars_generated_smiles.txt').readlines():
+    mol = Chem.MolFromSmiles(smiles.strip(), params=params)
+    rlist.append(RMol(mol))
 
-# generate conformers, etc
-rlist[5].generate_conformers(100)
-rec_final = prody.parsePDB("rec_final.pdb")
-rlist[5].remove_clashing_confs(rec_final)
-rlist[5].to_file('exampleIndex5.sdf')
+# ensure that the common core is indeed that
+for rmol in rlist:
+    # check if the core_mol is a substructure
+    if not rmol.HasSubstructMatch(core):
+        raise Exception('The core molecule is not a substructure of one of the RMols in the RList, '
+                        'according to Mol.HasSubstructMatch')
+    rmol._save_template(core)
+
+# conformers and energies, example for one molecule
+mol_id = 1 # 5 broken, wrong number of atoms
+rlist[mol_id].generate_conformers(num_conf=100, minimum_conf_rms=0.5)
+protein_filename = "rec_final.pdb"
+rec_final = prody.parsePDB(protein_filename)
+rlist[mol_id].remove_clashing_confs(rec_final)
+
+# continue only if there are any conformers to be optimised
+if rlist[mol_id].GetNumConformers() > 0:
+    energies = rlist[mol_id].optimise_in_receptor(
+        receptor_file=protein_filename,
+        ligand_force_field="openff",
+        use_ani=True,
+        sigma_scale_factor=0.8,
+        relative_permittivity=4,
+        water_model=None,
+        platform_name='CPU',
+    )
+    rlist[mol_id].sort_conformers(energy_range=5)
+
+    affinities = rlist[mol_id].gnina(receptor_file=protein_filename)
+
+    with Chem.SDWriter(f'Rmol{mol_id}_best_conformers.sdf') as SDW:
+        SDW.write(rlist[mol_id])
