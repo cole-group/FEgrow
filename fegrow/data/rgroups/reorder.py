d008b5498f80ace537dcdd1279258f30f2b265dc
bieniekmat <bieniekmat@gmail.com>
2023-11-03 14:46:22 +0000

    ordered by rank

---
+++ fegrow/data/rgroups/reorder.py
@@ -0,0 +1,26 @@
+from rdkit import Chem
+from bs4 import BeautifulSoup
+
+soup = BeautifulSoup(open('top500_R_replacements.xml').read(), 'html.parser')
+
+lib = list(Chem.SDMolSupplier('library.sdf', removeHs=False))
+
+for mol in lib:
+	sm = Chem.MolToSmiles(Chem.RemoveHs(mol))
+
+	found = False
+	for top in soup.find_all('center'):
+		refsm = Chem.MolToSmiles(Chem.MolFromSmiles(top.attrs['smiles']))
+		if sm == refsm:
+			found = True
+			mol.SetIntProp('rank', int(top.attrs['degree']))
+			break
+	if found == False:
+		print('Not found', sm)
+		mol.SetIntProp('rank', -1)
+
+reordered_mols = sorted(lib, reverse=True, key=lambda m:m.GetIntProp('rank'))
+
+with Chem.SDWriter('reordered.sdf') as SD:
+	for mol in reordered_mols:
+		SD.write(mol)
