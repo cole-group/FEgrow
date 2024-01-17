bd89b7b4c8e2da12047474cd6a4e5cbaf9ee97cc
Josh Horton <Josh.Horton@newcastle.ac.uk>
2024-01-17 13:40:02 +0000

    add CLI scoring prototype

---
+++ fegrow/cli/cli.py
@@ -0,0 +1,30 @@
+import pathlib
+
+import click
+from fegrow.cli.scoring import score
+from fegrow.cli.utils import Settings
+
+
+@click.group()
+def cli():
+    pass
+
+
+cli.add_command(score)
+
+
+@cli.command()
+@click.option(
+    "-g",
+    "--gnina-path",
+    type=click.Path(exists=True, file_okay=True, dir_okay=False, executable=True),
+    help="The path to the gnina executable which will override the settings.",
+)
+def settings(gnina_path: pathlib.Path):
+    """
+    Create a runtime settings object for scoring runs which can be configured.
+    """
+    config = Settings(gnina_path=gnina_path)
+    with open("settings.json") as output:
+        output.write(config.json(indent=2))
+
