99e6c211e7751282b0edbf9b0b844f95a8f51cfb
bieniekmat <bieniekmat@gmail.com>
2024-02-09 17:08:17 +0000

    Setting a model via a property.

--- fegrow/testing/test_chemspace.py
+++ fegrow/testing/test_chemspace.py
@@ -243,6 +243,46 @@ def test_al_local(RGroups, sars_scaffold_chunk_sdf, rec_7l10_final_path):
         print(f"AL cycle cnnaffinity. Mean: {res.score.mean():.2f}, Min: {res.score.min():.2f}, Max: {res.score.max():.2f}")
 
 
+def test_al_manual_gp(RGroups, sars_scaffold_chunk_sdf, rec_7l10_final_path):
+    """
+
+    """
+    from fegrow.al import AL
+    # check if two molecules were built with chemspace
+    chemspace = ChemSpace()
+
+    scaffold = Chem.SDMolSupplier(str(root / "data/5R83_core.sdf"), removeHs=False)[0]
+    chemspace.add_scaffold(scaffold, 6)
+
+    oracle = pandas.read_csv(root / "data/cs50k_scored49578_unique47710.csv.zip")
+
+    # separate the Smiles to be scanned
+    smiles_list = oracle.Smiles.to_list()[:40]
+    chemspace.add_smiles(smiles_list, h=6)
+
+    # the protein here does not matter as we don't use it anyway
+    chemspace.add_protein(rec_7l10_final_path)
+
+    def oracle_look_up(scaffold, h, smiles, *args, **kwargs):
+        # mol, data
+        return None, {"score": oracle[oracle.Smiles == smiles].iloc[0].cnnaffinity}
+
+    # select random molecules
+    random_pics = chemspace.active_learning(n_instances=5, first_random=True)
+    chemspace.evaluate(random_pics, full_evaluation=oracle_look_up)
+
+    # configure active learning
+    chemspace.model = AL.get_gaussian_process_estimator()
+
+    # set the results for the studied smiles
+    for i in range(2):
+        picks = chemspace.active_learning(n_instances=5)
+        res = chemspace.evaluate(picks, full_evaluation=oracle_look_up)
+        # filter out the penalties
+        res = res[res.score != 0]
+        print(f"AL cycle cnnaffinity. Mean: {res.score.mean():.2f}, Min: {res.score.min():.2f}, Max: {res.score.max():.2f}")
+
+
 @pytest.mark.skip(reason="requires the pydockingorg interface. ")
 def test_adding_enamines(RGroups, sars_scaffold_chunk_sdf, rec_7l10_final_path):
     """
