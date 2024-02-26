02def3dc292029b159f4d7263e32e52a8288fb99
bieniekmat <bieniekmat@gmail.com>
2024-02-26 11:52:09 +0000

    - Preparing for testing of Smiles

--- fegrow/testing/__init__.py
+++ fegrow/testing/__init__.py
@@ -1,3 +1,10 @@
 import pathlib
 
-loc = str(pathlib.Path(__file__).parent)
+loc = pathlib.Path(__file__).parent
+
+# for the tutorial, simplify the access to the scaffold and the smiles for it
+core_5R83_path = str(loc / 'data' / '5R83_core.sdf')
+rec_5R83_path = str(loc / 'data' / '5R83_rec.pdb')
+smiles_5R83_core_path = str(loc / 'data' / 'cs50k_scored49578_unique47710.csv.zip')
+
+loc = str(loc)
